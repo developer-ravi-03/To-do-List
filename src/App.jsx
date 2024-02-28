20be64ddd8b1514396256a0157354408ecf4fae1
Ravi Kumar <ravibseb96@gmail.com>
2024-02-29 01:10:01 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,123 @@
+import { useEffect, useState } from 'react'
+import React from 'react'
+import Navbar from './components/Navbar'
+import { v4 as uuidv4 } from 'uuid';
+import { FaRegEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+
+
+function App() {
+
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [showFinished, setshowFinished] = useState(true);
+
+  useEffect(() => {
+    let todoString = localStorage.getItem("todos")
+    if (todoString) {
+      let todos = JSON.parse(localStorage.getItem("todos"));
+      setTodos(todos)
+
+    }
+
+  }, [])
+
+
+  const saveToLS = (params) => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }
+
+  const toggleFinished = (e) => {
+    setshowFinished(!showFinished)
+  }
+
+
+
+  const handleEdit = (e, id) => {
+    let t = todos.filter(i => i.id === id)
+    setTodo(t[0].todo)
+    let newTodos = todos.filter(item => {
+      return item.id !== id
+    });
+    setTodos(newTodos)
+    saveToLS();
+  }
+
+  const handleDelete = (e, id) => {
+    let newTodos = todos.filter(item => {
+      return item.id !== id
+    });
+    setTodos(newTodos)
+    saveToLS();
+  }
+
+  const handleAdd = () => {
+    setTodos([...todos, { id: uuidv4(), todo, isCompleted: false }])
+    setTodo("")
+    console.log(todos);
+    saveToLS();
+
+  }
+
+  const handleChange = (e) => {
+    setTodo(e.target.value)
+  }
+
+  const handleCheckbox = (e) => {
+    let id = e.target.name;
+    let index = todos.findIndex(item => {
+      return item.id === id;
+    })
+    let newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos)
+    saveToLS();
+  }
+
+
+
+
+  return (
+    <>
+      <Navbar />
+      <div className="mx-3 md:container md:mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh] md:w-1/2">
+        <h1 className='font-bold text-center text-xl'>iTask - Manage your todos at one place</h1>
+        <div className='addTodo my-5 flex flex-col gap-4'>
+          <h2 className='text-lg font-bold'>
+            Add a Todo
+          </h2>
+
+          <input onChange={handleChange} value={todo} type="text" className='w-full border rounded-full p-5 py-2' />
+          <button onClick={handleAdd} disabled={todo.length <= 2} className='bg-violet-800 hover:bg-violet-950 disabled:bg-violet-500 p-2 py-1 text-sm font-bold text-white rounded-md'>
+            Save
+          </button>
+        </div>
+        <input className='my-4' onChange={toggleFinished} type="checkbox" checked={showFinished} />
+        <label htmlFor="show" className="mx-2">Show Finished</label>
+        <div className='h-[1px] bg-black opacity-15 w-3/3 my-5 mx-auto'></div>
+        <h2 className='text-lg font-bold'>Your Todos</h2>
+        <div className='todos'>
+          {todos.length === 0 && <div className='m-5'>No Todos to display</div>}
+          {todos.map(item => {
+            return (showFinished || !item.isCompleted) && <div key={item.id} className="todo flex w-3/2 justify-between">
+              <div className='flex gap-5'>
+                <input name={item.id} onChange={handleCheckbox} type="checkbox" checked={item.isCompleted} id='' />
+                <div className={item.isCompleted ? "line-through mt-1" : "mt-1"}>{item.todo}</div>
+              </div>
+              <div className="buttons flex h-full">
+                <button onClick={(e) => { handleEdit(e, item.id) }} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1 my-1'>
+                  <FaRegEdit />
+                </button>
+                <button onClick={(e) => { handleDelete(e, item.id) }} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1 my-1'>
+                  <MdDelete />
+                </button>
+              </div>
+            </div>
+          })}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
